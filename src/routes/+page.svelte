652501a7f7e11e1557b631084a52a21d7a20187a
dragonflyinmypool <jheiney10@gmail.com>
2023-01-12 12:28:55 -0500

    added svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Header from '$lib/header.svelte';
+	import Form from '$lib/form.svelte';
+	import Results from '$lib/results.svelte';
+</script>
+
+<main>
+	<Header />
+	<!-- Form -->
+	<Form />
+	<!-- Submit -->
+	<div class="submit bubleBox">
+		<div>SUBMIT</div>
+	</div>
+	<!-- Results -->
+	<!-- <Results /> -->
+</main>
+
+<style>
+	.submit {
+		background-color: var(--medium-aquamarine);
+		color: var(--midnight-green-eagle-green);
+		font-weight: bold;
+		font-family: 'Heebo', sans-serif;
+		border-radius: 20px;
+		text-align: center;
+		width: 100px;
+	}
+</style>
